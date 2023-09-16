fcc96f520694a7888d7028549a3e543a2e33de2c
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2023-09-16 19:34:33 +0300

    hadeth tab hadeth details

---
+++ lib/hadeth_details/HadethDetailsScreen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c9_sat/home/hadeth/Hadeth.dart';
+
+class HadethDetailsScreen extends StatelessWidget {
+  static const String routeName = "hadeth-details";
+
+  @override
+  Widget build(BuildContext context) {
+    Hadeth args = ModalRoute.of(context)?.settings.arguments as Hadeth;
+    return Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/main_background.jpg',
+                ),
+                fit: BoxFit.fill)),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(args.title),
+          ),
+          body: Card(
+            elevation: 18,
+            margin: EdgeInsets.symmetric(vertical: 64, horizontal: 18),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Text(
+                      args.content,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 24, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/home/hadeth/HadethNameWidget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c9_sat/hadeth_details/HadethDetailsScreen.dart';
+import 'package:islami_c9_sat/home/hadeth/Hadeth.dart';
+
+class HadethNameWidget extends StatelessWidget {
+  Hadeth hadeth;
+
+  HadethNameWidget(this.hadeth);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .pushNamed(HadethDetailsScreen.routeName, arguments: hadeth);
+      },
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(vertical: 4),
+        child: Text(
+          hadeth.title,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+}
